fe1d59db81fa61a155cf9814b270f869d45abc81
Alessandro Pereira Torres De Sá Neto <alessandroptsn@yahoo.com.br>
2022-07-17 19:19:52 -0300

    Update main.py

--- source/api/main.py
+++ source/api/main.py
@@ -13,15 +13,6 @@ import os
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-#import pathlib
-#from pathlib import Path
-#files = sorted(pathlib.Path('.').glob('**/haarcascade_frontalface_default.xml'))
-
-#for i in files:
-#     a = i
-
-
-
 # name of the model artifact
 artifact_model_name = "emotions/model_export:latest"
 
@@ -35,41 +26,20 @@ best_model = wandb.restore('model.h5', run_path="alessandroptsn/emotions/skt69t8
 modelwb = load_model(best_model.name)
 
 
-
-
-#def read_imagefile(file) -> Image.Image:
-#    image = Image.open(BytesIO(file))
-#    image = np.array(image).astype('float32')
-#    return image
-     
-     
-face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-#face_cascade = cv2.CascadeClassifier(str(a))
+face_cascade = cv2.CascadeClassifier("/app/haarcascade_frontalface_default.xml")
 
 def load2(ft):
-#    ft = read_imagefile(ft)
-     foto=cv2.cvtColor(ft, cv2.COLOR_BGR2RGB)
-     faces = face_cascade.detectMultiScale(foto, 1.3, 3)
-     #if faces == ():
-     #     color=cv2.resize(foto,(20,20))
-     if 1>2:
-          print("lol")
-     else:
-          for (x,y,w,h) in faces:
-               cv2.rectangle(foto, (x,y), (x+w, y+h), (0,0,255), 2)
-               color = foto[y:y+h, x:x+w]
-          color=cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
-          if color[1,1,0] == 255:
-               color=cv2.resize(color,(20,20))
-     return color
+   foto_=cv2.cvtColor(ft, cv2.COLOR_BGR2RGB)
+   foto=cv2.cvtColor(ft, cv2.COLOR_BGR2RGB)
+   faces = face_cascade.detectMultiScale(foto, 1.3, 3)
+   for (x,y,w,h) in faces:
+       cv2.rectangle(foto, (x,y), (x+w, y+h), (0,0,255), 2)
+       color = foto[y:y+h, x:x+w]
+   color=cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
+   #if color[1,1,0] == 255:
+   color=cv2.resize(color,(20,20))
+   return color
       
-     
-     
-def prepare(image):
-    IMG_SIZE = 600
-    new_array = cv2.resize(image, (IMG_SIZE, IMG_SIZE)) 
-    return new_array.reshape(-1, IMG_SIZE,IMG_SIZE,3)
-     
 #def load(filename):
 #   np_image = Image.open(io.BytesIO(filename)) 
 #   np_image = np.array(np_image).astype('float32')
@@ -89,35 +59,21 @@ async def say_hello():
 # run the model inference and use a face data structure via POST to the API.
 @app.post("/face") 
 async def root(file: UploadFile = File(...)):
-    img = await file.read()
-    img = np.fromstring(img, np.uint8)
-    images = cv2.imdecode(img, cv2.IMREAD_COLOR).astype(np.float32)
-    #images = np.fromstring(img, np.uint8)
-    #images = cv2.imdecode(img, cv2.IMREAD_COLOR)
-    #images = np.array(Image.open(img))
-    
-    #images = np.array(Image.open(img))
-    #images = np.fromstring(Image.open(img), np.uint8)
-    #images = read_imagefile(img)
-    
-    #foto=cv2.cvtColor(images, cv2.COLOR_BGR2RGB)
-    #faces = face_cascade.detectMultiScale(foto, 1.3, 3)
-    #if faces == ():
-    if 1>2:
+    img = await  file.read()
+    prediction = np.around(modelwb.predict(load2(cv2.imread(img))), decimals=2)
+    string = ','.join(str(x) for x in prediction)
+    if string == "[1. 0. 0. 0. 0. 0.]":
+        result = "Surprise"
+    elif string == "[0. 1. 0. 0. 0. 0.]":
+        result = "Sad"
+    elif string == "[0. 0. 1. 0. 0. 0.]":
+        result = "Neutral"        
+    elif string == "[0. 0. 0. 1. 0. 0.]":
+        result = "Happy"
+    elif string == "[0. 0. 0. 0. 1. 0.]":
+        result = "Fear"
+    elif string == "[0. 0. 0. 0. 0. 1.]":
+        result = "Angry"    
+    else:     
         result = "Unrecognized Face"
-    else:
-          prediction = np.around(modelwb.predict(load2(prepare(images))), decimals=2)
-          string = ','.join(str(x) for x in prediction)
-          if string == "[1. 0. 0. 0. 0. 0.]":
-               result = "Surprise"
-          elif string == "[0. 1. 0. 0. 0. 0.]":
-               result = "Sad"
-          elif string == "[0. 0. 1. 0. 0. 0.]":
-               result = "Neutral"        
-          elif string == "[0. 0. 0. 1. 0. 0.]":
-               result = "Happy"
-          elif string == "[0. 0. 0. 0. 1. 0.]":
-               result = "Fear"
-          elif string == "[0. 0. 0. 0. 0. 1.]":
-               result = "Angry"    
     return result
